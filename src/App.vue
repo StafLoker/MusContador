519d6e7da405cf13501fcd7f6680093ff91ec381
StafLoker <staf.loker@gmail.com>
2024-05-20 18:29:06 +0200

    feat: complete footer

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,20 @@
 v-app
   v-main
     v-container
-
+      v-row.justify-center
+        h1 MusContador
     v-footer
       span MusContador
+      v-spacer
+      span 2024
+      v-spacer
+      a(
+        href="https://github.com/StafLoker/MusContador"
+        target="_blank"
+      )
+        v-icon(
+          icon="mdi-github"
+        )
 </template>
 
 <script lang="ts" setup>
