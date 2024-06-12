a27e7fb282d2ce0a79334bfa0e841e8d84fff641
StafLoker <staf.loker@gmail.com>
2024-06-12 19:57:02 +0200

    feat: use FieldPoint component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,45 @@ v-app
     v-container
       v-row.justify-center
         h1 MusContador
+      v-row
+        v-col
+          FieldPoint(
+            header="A",
+            :value="0"
+          )
+        v-col
+          FieldPoint(
+            header="B",
+            :value="0"
+          )
+      v-row
+        v-col
+          FieldPoint(
+            header="Grande",
+            :value="0"
+          )
+        v-col
+          FieldPoint(
+            header="Chicas",
+            :value="0"
+          )
+        v-col
+          FieldPoint(
+            header="Pares",
+            :value="0"
+          )
+        v-col
+          FieldPoint(
+            header="Juego",
+            :value="0"
+          )
+      v-row
+        v-col
+          v-btn A
+        v-col
+          v-btn B
+      v-row
+        v-btn Reiniciar
     v-footer
       span MusContador
       v-spacer
